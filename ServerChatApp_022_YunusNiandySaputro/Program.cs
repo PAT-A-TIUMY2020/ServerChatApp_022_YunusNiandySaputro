cd54d288bdabd0c54f9f05cbd76b7de3f22eb26a
ReAlpha39 <yunusw718@gmail.com>
2021-01-08 23:53:05 +0800

    add service and open it when app running

--- ServerChatApp_022_YunusNiandySaputro/Program.cs
+++ ServerChatApp_022_YunusNiandySaputro/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using WCFService_2Way_022;
 
 namespace ServerChatApp_022_YunusNiandySaputro
 {
@@ -10,12 +12,23 @@ namespace ServerChatApp_022_YunusNiandySaputro
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
